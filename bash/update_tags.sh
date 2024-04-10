f046a117fd6a9e7be31160df959b7aae848f4ed5
Victor Leiva <victor.leiva@outlook.com>
2024-04-10 13:29:49 +0200

    apply tag to rg

--- bash/update_tags.sh
+++ bash/update_tags.sh
@@ -3,7 +3,12 @@ resourcegroup='az-resources'
 resources=$(az resource list -g $resourcegroup -o json)  
 tag="client=client1"
 
+# Apply tag to resource group
+rgid=$(az group show --resource-group $resourcegroup -o json | jq -r '.id')
 
+az resource tag --tags $tag --ids $rgid
+
+# Apply tag to all resources within the resource group
 for resource in $(echo "$resources" | jq -c '.[]'); do
 
     name=$(echo "$resource" | jq -r '.name')
