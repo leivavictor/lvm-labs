9d104103f7aef4e43239b2783e58ae5d92fa2f4f
Victor Leiva <victor.leiva@outlook.com>
2024-04-10 14:05:18 +0200

    replacing to id

--- bash/update_tags.sh
+++ bash/update_tags.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-resourcegroup='az-resources'
+resourcegroup='rg'
 resources=$(az resource list -g $resourcegroup -o json)  
-tag="client=client1"
+tag="Client=clientx"
 
 # Apply tag to resource group
 rgid=$(az group show --resource-group $resourcegroup -o json | jq -r '.id')
 
-az resource tag --tags $tag --ids $rgid
+az resource tag --tags $tag --ids $rgid --is-incremental
 
 # Apply tag to all resources within the resource group
 for resource in $(echo "$resources" | jq -c '.[]'); do
 
-    name=$(echo "$resource" | jq -r '.name')
+    id=$(echo "$resource" | jq -r '.id')
     
-    resourcetype=$(echo "$resource" | jq -r '.type')
+    echo "$id"
 
-    if az resource tag --tags $tag -g $resourcegroup -n $name --resource-type $resourcetype --is-incremental; then
+    if az resource tag --tags $tag --ids $id --is-incremental; then
 
         echo "Success"
     else
-        echo "Error applying tags to $name"
+        echo "Error applying tags to $id"
     fi
  
 done
